7fb002ac7411f76d1bdcec6b0821cc3299b8e029
brenommelo <brenommelo@gmail.com>
2024-09-22 10:53:35 -0300

    feat: start routs

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/brenommelo/rssagg
 
 go 1.23.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Ola breno")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the enviroment")
+	}
+	fmt.Println("PORT", portString)
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port: ", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
